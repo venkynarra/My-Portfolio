50c683a3be42a1ec850b0f946da5dc62b6569d90
venkatesh <venkateshnarra368@gmail.com>
2025-05-05 11:56:07 -0500

    Day 22- added description, images and links on both side of columns.

--- main.py
+++ main.py
@@ -19,15 +19,22 @@ content2 = """
     """
 st.write(content2)
 
-col3, col4 = st.columns(2)
+
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])# the integres are ration for width of the columns
 
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[0:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[source code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
-
+        st.write(row["description"])
+        st.image("images/" + row["image"]) # to print each image we are concatinating.
+        st.write(f"[source code]({row['url']})")
